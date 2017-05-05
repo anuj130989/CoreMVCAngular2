f6d3adf9bf29daab91fa91dcb6e4541d49c77880
Arora <Arora@ANUJ>
2017-05-05 21:47:16 +0530

    Add project files.

---
+++ src/CoreMVCAngular/gulpfile.js
@@ -0,0 +1,50 @@
+﻿/// <binding Clean='copy-all, copy-es6-shim' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+"use strict";
+
+var gulp = require("gulp");
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var root_path = { webroot: "./wwwroot/" };
+
+//library source
+root_path.nmSrc = "./node_modules/";
+
+//library destination
+root_path.package_lib = root_path.webroot + "lib-npm/";
+
+gulp.task("copy-systemjs", function () {
+    return gulp.src(root_path.nmSrc + '/systemjs/dist/**/*.*', {
+        base: root_path.nmSrc + '/systemjs/dist/'
+    }).pipe(gulp.dest(root_path.package_lib + '/systemjs/'));
+});
+
+gulp.task("copy-angular2", function () {
+    return gulp.src(root_path.nmSrc + '/@angular/**/*.js', {
+        base: root_path.nmSrc + '/@angular/**'
+    }).pipe(gulp.dest(root_path.package_lib + '/@angular/*'));
+});
+
+gulp.task("copy-shim", function () {
+    return gulp.src(root_path.nmSrc + '/core-js/**/*.js', {
+        base: root_path.nmSrc + '/core-js/**/'
+    }).pipe(gulp.dest(root_path.package_lib + '/shim/*'));
+});
+
+gulp.task("copy-zone", function () {
+    return gulp.src(root_path.nmSrc + '/zone.js/**/*.js', {
+        base: root_path.nmSrc + '/zone.js/**/'
+    }).pipe(gulp.dest(root_path.package_lib + '/zone/*'));
+});
+
+gulp.task("copy-rxjs", function () {
+    return gulp.src(root_path.nmSrc + '/rxjs/**/*.*', {
+        base: root_path.nmSrc + '/rxjs/**/'
+    }).pipe(gulp.dest(root_path.package_lib + '/rxjs/*'));
+});
+
+gulp.task("copy-all", ["copy-rxjs", 'copy-angular2', 'copy-systemjs', 'copy-shim', 'copy-zone']);
---
+++ src/CoreMVCAngular/wwwroot/app/app.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'core-app',
+    templateUrl: 'app/app.component.html',    
+})
+export class AppComponent {
+    public test: string = 'hello';
+
+}
