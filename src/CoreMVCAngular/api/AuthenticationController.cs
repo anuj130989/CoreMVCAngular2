6f1ff2bc7372d3329506649927dc7ed55ef28405
Anuj <anuj130989@gmail.com>
2017-06-20 21:26:28 +0530

    Changes in authentication controller

---
+++ src/CoreMVCAngular/api/AuthenticationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreMVCAngular.api
+{
+    [Route("api/[controller]")]
+    public class AuthenticateController : Controller
+    {
+        /// <summary>
+        /// Authenticate a user for provided username and password
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Fetch")]
+        public string Authenticate(string userName, string password)
+        {
+            return "login success";
+        }
+
+    }
+}
